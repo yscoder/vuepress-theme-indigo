40e2d57061f8c0def7ca2dded4ce1a6110500b67
yscoder <634206017@qq.com>
2018-04-27 00:13:54 +0800

    add mixin posts, postlist

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,8 +1,35 @@
+import config from './config'
+
 const Layout = () => import('./theme/Layout')
+const themeConfig = config.themeConfig
+const slugReg = /\/([^\/]+).html$/
 
 export default ({ Vue, options, router, siteData }) => {
   // TODO: 尝试不依赖已有内置数据，在这里对 siteData 解析、组装成自己需要的数据结构
   // Example： { postList: [], posts: {}, tagList: [], tags: { }  }
+
+  // console.log(siteData)
+  const postList = []
+  const posts = {}
+  const postDir = themeConfig.postDir
+  siteData.pages.forEach(page => {
+    if (page.path.indexOf(postDir) === 0) {
+      const slug = page.path.match(slugReg)[1]
+      postList.push(slug)
+      posts[slug] = { ...page, slug }
+    }
+  })
+
+  Vue.mixin({
+    data() {
+      return {
+        $blog: {
+          postList,
+          posts
+        }
+      }
+    }
+  })
 
   router.addRoutes([
     {

560369e9af55c8dfcb43fd183467bbad770d4c77
unknown <634206017@qq.com>
2018-04-27 15:55:59 +0800

    'build'

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -21,15 +21,11 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "f44118c7a6acf1e7c157fda73712af3e"
+    "revision": "1f1b994e443c1ae480d8610aca5e6aad"
   },
   {
-    "url": "404/index.html",
-    "revision": "af4980ad842a387c573b9781a9d865c7"
-  },
-  {
-    "url": "assets/css/8.styles.b90bd0ea.css",
-    "revision": "8bb21bc355fc390e3bc3563921a51082"
+    "url": "assets/css/7.styles.a6f0ba3d.css",
+    "revision": "f3fb3a64a7140d25502a617f1bd4afbe"
   },
   {
     "url": "assets/fonts/fontawesome-webfont.af7ae505.woff2",
@@ -108,180 +104,72 @@ self.__precacheManifest = [
     "revision": "734f817bbb181d0180d7b37749769cc0"
   },
   {
-    "url": "assets/js/0.e844ef25.js",
-    "revision": "ced33c0fa39fcb0097b011781bcc7616"
+    "url": "assets/js/0.1e646029.js",
+    "revision": "a9375c18b79c18a785ca943d689cb845"
   },
   {
-    "url": "assets/js/1.15ef5707.js",
-    "revision": "6f0becfa3a6b1f9877e98f36645e7d5d"
+    "url": "assets/js/1.e14feefe.js",
+    "revision": "a1a5a12dd2e3ac8ee2467e3e1dab0482"
   },
   {
-    "url": "assets/js/2.5eb434c3.js",
-    "revision": "537eb3844d45d88fbee29ee1d64e618a"
+    "url": "assets/js/2.dc733e03.js",
+    "revision": "45ddf4239d0103a905ce684d03555024"
   },
   {
-    "url": "assets/js/3.3780d7e5.js",
-    "revision": "98c50baa58e5ec9fe927bef5647ff5ad"
+    "url": "assets/js/3.67fffaa8.js",
+    "revision": "bd4a1636ff1f9a839286d15a1af96776"
   },
   {
-    "url": "assets/js/4.4628f922.js",
-    "revision": "41c6ddf649f505ce57f6f97318c5938b"
+    "url": "assets/js/4.6f875459.js",
+    "revision": "509a83175cc146ce2456e974114fabac"
   },
   {
-    "url": "assets/js/5.33f58b32.js",
-    "revision": "ccd38b7468ef1103345631f3b63d2b00"
+    "url": "assets/js/5.7396f4af.js",
+    "revision": "ddd8c199ce118c145196975aaba8b1ef"
   },
   {
-    "url": "assets/js/6.8645a6ac.js",
-    "revision": "9c7c045d4e82958a7dd1be85533ec765"
+    "url": "assets/js/6.95b42c69.js",
+    "revision": "0c64761e375ff6f9d4e8b6e7691a011d"
   },
   {
-    "url": "assets/js/7.92a6b3da.js",
-    "revision": "6947e6f1a0f1b4d9054f953220232537"
-  },
-  {
-    "url": "assets/js/app.534f1149.js",
-    "revision": "2401087cc94246603a417971714c6e0c"
-  },
-  {
-    "url": "brand.jpg",
-    "revision": "734f817bbb181d0180d7b37749769cc0"
+    "url": "assets/js/app.db11ad3c.js",
+    "revision": "9f4aeb534f3a6055a1e97207f059146a"
   },
   {
     "url": "face.jpg",
     "revision": "55c9d89de66c7f697ead1b3ac10b008e"
   },
   {
-    "url": "fonts/fontawesome/fontawesome-webfont.eot",
-    "revision": "674f50d287a8c48dc19ba404d20fe713"
-  },
-  {
-    "url": "fonts/fontawesome/fontawesome-webfont.svg",
-    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606"
-  },
-  {
-    "url": "fonts/fontawesome/fontawesome-webfont.ttf",
-    "revision": "b06871f281fee6b241d60582ae9369b9"
-  },
-  {
-    "url": "fonts/fontawesome/fontawesome-webfont.woff",
-    "revision": "fee66e712a8a08eef5805a46892932ad"
-  },
-  {
-    "url": "fonts/fontawesome/fontawesome-webfont.woff2",
-    "revision": "af7ae505a9eed503f8b8e6982036873e"
-  },
-  {
-    "url": "fonts/fontawesome/FontAwesome.otf",
-    "revision": "0d2717cd5d853e5c765ca032dfd41a4d"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Bold.eot",
-    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Bold.ttf",
-    "revision": "e31fcf1885e371e19f5786c2bdfeae1b"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Bold.woff",
-    "revision": "dc81817def276b4f21395f7ea5e88dcd"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Bold.woff2",
-    "revision": "39b2c3031be6b4ea96e2e3e95d307814"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Light.eot",
-    "revision": "a990f611f2305dc12965f186c2ef2690"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Light.ttf",
-    "revision": "46e48ce0628835f68a7369d0254e4283"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Light.woff",
-    "revision": "3b813c2ae0d04909a33a18d792912ee7"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Light.woff2",
-    "revision": "69f8a0617ac472f78e45841323a3df9e"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Medium.eot",
-    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Medium.ttf",
-    "revision": "894a2ede85a483bf9bedefd4db45cdb9"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Medium.woff",
-    "revision": "fc78759e93a6cac50458610e3d9d63a0"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Medium.woff2",
-    "revision": "574fd0b50367f886d359e8264938fc37"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Regular.eot",
-    "revision": "30799efa5bf74129468ad4e257551dc3"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Regular.ttf",
-    "revision": "df7b648ce5356ea1ebce435b3459fd60"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Regular.woff",
-    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Regular.woff2",
-    "revision": "2751ee43015f9884c3642f103b7f70c9"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Thin.eot",
-    "revision": "dfe56a876d0282555d1e2458e278060f"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Thin.ttf",
-    "revision": "94998475f6aea65f558494802416c1cf"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Thin.woff",
-    "revision": "7500519de3d82e33d1587f8042e2afcb"
-  },
-  {
-    "url": "fonts/roboto/Roboto-Thin.woff2",
-    "revision": "954bbdeb86483e4ffea00c4591530ece"
+    "url": "favicon.png",
+    "revision": "cfa97d05be7622e0f57799d7149b93f0"
   },
   {
     "url": "index.html",
-    "revision": "08959185c2e6a40dda771ff0517b98c9"
+    "revision": "e6d8996fdb75fb5dca93c4bf7631ef16"
   },
   {
     "url": "posts/cursor-offset-at-input.html",
-    "revision": "7d7fa8a4ca3c9d3a9c42ccc157d9315e"
+    "revision": "a9a21e16708c2118682bffb3431ef035"
   },
   {
     "url": "posts/text-truncation.html",
-    "revision": "4e75e375fdd3c0b0a6e1b0e62c4cdcc7"
+    "revision": "355e4ed1b902af1a62b2999a012cfdfd"
   },
   {
     "url": "posts/vue-best-practices.html",
-    "revision": "3040a4aede7b8e185195cdc93adc12d2"
+    "revision": "875f0a8ffa1ebdec97c8ade1f161dd17"
   },
   {
     "url": "posts/webpack-use-lodash.html",
-    "revision": "b3997b1f71225b2113e51810c4aea1c2"
+    "revision": "b722d73e8c0917747cf0edfb94ed2f66"
   },
   {
     "url": "posts/write-good-front-end-component.html",
-    "revision": "d62313a9330802b05a7959aa7775d776"
+    "revision": "365eb11d52d608a8ae1c4b925b37771e"
   },
   {
     "url": "tags/index.html",
-    "revision": "6ec9963bbc9a290acf98a0b88a610c62"
+    "revision": "0ce03c6eb93242d9913b65a88cda649f"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();

345f4a5c607a66a695e9bd4b982e86ce73215ef4
yscoder <634206017@qq.com>
2018-05-01 01:41:17 +0800

    add copy link

--- src/.vuepress/theme/components/Share.vue
+++ src/.vuepress/theme/components/Share.vue
@@ -20,7 +20,22 @@
         </v-list-tile-avatar>
         <v-list-tile-title class="capitalize">{{$tt(item.text)}}</v-list-tile-title>
       </v-list-tile>
+      <v-list-tile ripple
+                   color="secondary"
+                   key="copy"
+                   @click="copyLink">
+        <v-list-tile-avatar>
+          <i class="fa fa-lg fa-copy"></i>
+        </v-list-tile-avatar>
+        <v-list-tile-title>{{$tt('copyLink')}}</v-list-tile-title>
+      </v-list-tile>
     </v-list>
+    <input type="text"
+           :value="url"
+           ref="copyEl"
+           tabindex="-1"
+           aria-hidden="true"
+           class="fake-hide">
   </v-menu>
 </template>
 <script>
@@ -32,10 +47,18 @@ export default {
   },
   data() {
     return {
-      socials: null
+      socials: null,
+      url: ''
     }
   },
   methods: {
+    copyLink() {
+      const $el = this.$refs.copyEl
+      $el.focus()
+      $el.select()
+      document.execCommand('copy')
+      $el.blur()
+    }
   },
   beforeMount() {
     const customSocials = this.$site.themeConfig.socials
@@ -45,6 +68,7 @@ export default {
         const { title, excerpt: summary } = this.$page
         const origin = location.origin + this.$site.base
         const pic = ''
+        this.url = url
         this.socials = customSocials.map(item => {
           const { icon, share } = defaultSocials[item]
           const href = share({ url, title, summary, origin, pic })
@@ -57,5 +81,3 @@ export default {
   }
 }
 </script>
-<style lang="stylus">
-</style>
--- src/.vuepress/theme/languages/zh-CN.js
+++ src/.vuepress/theme/languages/zh-CN.js
@@ -1,5 +1,6 @@
 module.exports = {
   license: `博客内容遵循 <a rel="license noopener noreferrer" href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh" target="_blank">知识共享 署名 - 非商业性 - 相同方式共享 4.0 国际协议</a>`,
   postNav_prev: 'Prev',
-  postNav_next: 'Next'
+  postNav_next: 'Next',
+  copyLink: '复制链接'
 }
